84309899a4e90965f6c7f4e28149c9fbdc6dcc00
zuoyuan <zuoyuan@meituan.com>
2019-01-05 19:15:35 +0800

    add risk plot

--- bond-interest/interest.py
+++ bond-interest/interest.py
@@ -8,6 +8,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('data.csv', sep='\t')
+
+df_plt = pd.DataFrame()
+for name in ['中债国债收益率曲线', '中债商业银行普通债收益率曲线(AAA)', '中债中短期票据收益率曲线(AAA)']:
+    df_plti = df[df['曲线名称'] == name].drop('曲线名称', axis=1)
+    df_plt[name] = df_plti.sort_values(by='日期').set_index('日期')['3月']
+
+df_plt.plot(figsize=(12, 5), grid='on')
+plt.title(u'3月期收益率曲线')
+f = plt.gcf()
+f.savefig('data_risk.svg')
+# plt.show()
+
+
 df_plt = df[df['曲线名称'] == '中债国债收益率曲线'].drop('曲线名称', axis=1)
 
 df_plt.sort_values(by='日期').set_index('日期')[['3月', '1年', '10年']][-365:].plot(figsize=(12, 5), grid='on')
